dfcede7dff33dd0e96784827c497082e9fa73433
Jaewon <jungjaedev@gmail.com>
2022-03-23 16:57:32 +0900

    Add : Gravity function

--- index.js
+++ index.js
@@ -1,20 +1,46 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+const gravity = 1.5;
+
 class Player {
   constructor() {
     this.position = {
       x: 100,
       y: 100,
     };
-    this.width = 100;
-    this.height = 100;
+    this.velocity = {
+      x: 0,
+      y: 0,
+    };
+    this.width = 30;
+    this.height = 30;
   }
 
   draw() {
+    ctx.fillStyle = 'red';
     ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+
+  update() {
+    this.draw();
+    this.position.y += this.velocity.y;
+
+    if (this.position.y + this.height + this.velocity.y <= canvas.height) this.velocity.y += gravity;
+    else this.velocity.y = 0;
   }
 }
 
-const player = new player();
-player.draw();
+const player = new Player();
+player.update();
+
+function animate() {
+  requestAnimationFrame(animate);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  player.update();
+}
+
+animate();
